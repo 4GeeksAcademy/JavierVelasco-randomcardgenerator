72e4fd46389e2d051fc7b21c2266f415a908f360
Javier Velasco <133404180+JavierJVelasco786@users.noreply.github.com>
2023-06-10 00:58:14 +0000

    Updated

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("You see this message everytime the page loads!");
+
+  const suites = ["♥", "♣", "♠", "♦"];
+
+  function randomCard(arr) {
+    return Math.floor(math.random() * arr.lenght);
+  }
+
+  let suite = suite[randomCard(suite)];
+
+  let topsuite = document.querySelector("#top-left-suite");
+  topsuite.innerHTML = suite;
+
+  let bottomsuite = document.querySelector("#bottom-right-suite")
+  bottomsuite.innerHTML = suite;
+
+  console.log("This is the random number", randomCard(suites));
 };
+
+
+create an Array
+
+randomn Number
+
